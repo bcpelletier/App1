febed4794801928e8a94894527500bf216075d02
Boris Carlos Pelletier Gómez <Boris Carlos Pelletier Gmez>
2017-03-24 10:34:54 +0100

    Agregar archivos de proyecto.

---
+++ App1/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace App1
+{
+    [Activity(Label = "App1", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+                   
+            Button translateButton = FindViewById<Button>(Resource.Id.button1);
+            TextView t1 = FindViewById<TextView>(Resource.Id.textView1);
+                        
+            // Add code to translate number
+            string translatedNumber = string.Empty;
+
+            translateButton.Click += (object sender, EventArgs e) =>
+            {
+                // Translate user's alphanumeric phone number to numeric
+                t1.Text = "Hola";
+                SetContentView(Resource.Layout.layout1);
+            };
+
+        }
+
+
+    }
+}
+
